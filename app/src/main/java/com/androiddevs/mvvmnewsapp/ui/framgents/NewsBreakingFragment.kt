128d1b7e23c58b12c18ee28853f4ad52a0a59601
Saiful Islam Sajon <saifulislamsajon@gmail.com>
2020-04-21 01:00:04 +0600

    navigation graph has been created

--- app/src/main/java/com/androiddevs/mvvmnewsapp/ui/framgents/NewsBreakingFragment.kt
+++ app/src/main/java/com/androiddevs/mvvmnewsapp/ui/framgents/NewsBreakingFragment.kt
@@ -11,7 +11,7 @@ import com.androiddevs.mvvmnewsapp.R
 /**
  * A simple [Fragment] subclass.
  */
-class NewsBankingFragment : Fragment() {
+class NewsBreakingFragment : Fragment() {
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
